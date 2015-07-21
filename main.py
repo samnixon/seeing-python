7a657cd643a853a1cb611efd6322c8ba7b020c43
samnixon <monkeybss2@gmail.com>
2015-07-21 19:12:17 -0400

    Basic image io
    Can read and write images

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import numpy
 import cv2
 print cv2.__version__
-#something
+img = cv2.imread('batman1.jpg', 0)
+
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imwrite('grayscale_batman.png', img)
